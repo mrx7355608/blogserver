b1dc304d89e7dfbae53f95a7eb946c341c15a5d5
mrx7355608 <mrx7355608@gmail.com>
2023-12-09 22:00:04 +0500

    wrote data file for blog model

---
+++ src/data/blog.data.ts
@@ -0,0 +1,53 @@
+import { BlogModel } from "../models/blog.model";
+import { IBlog } from "../types/blog.types";
+
+const BlogData = () => {
+    async function findAll(skipVal: number) {
+        const blogs = await BlogModel.find().skip(skipVal).limit(10);
+        return blogs;
+    }
+
+    async function findByID(id: string) {
+        const blog = await BlogModel.findById(id);
+        return blog;
+    }
+
+    const findByTitle = async (title: string, skipVal: number) => {
+        const blogs = await BlogModel.find({ title }).skip(skipVal).limit(10);
+        return blogs;
+    };
+
+    async function findBySlug(slug: string) {
+        const blog = await BlogModel.find({ slug });
+        return blog;
+    }
+
+    async function create(data: IBlog) {
+        const newBlog = await BlogModel.create(data);
+        return newBlog;
+    }
+
+    async function updateByID(id: string, changes: Partial<IBlog>) {
+        const updatedBlog = await BlogModel.findByIdAndUpdate(id, changes, {
+            new: true,
+        });
+        return updatedBlog;
+    }
+
+    async function deleteByID(id: string) {
+        await BlogModel.findByIdAndDelete(id);
+        return null;
+    }
+
+    return {
+        findAll,
+        findByID,
+        findBySlug,
+        findByTitle,
+        create,
+        updateByID,
+        deleteByID,
+    };
+};
+
+export { BlogData };
