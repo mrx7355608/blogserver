b56ce4de80a4f6b166f2665c63256e5f7ed52664
mrx7355608 <mrx7355608@gmail.com>
2023-12-13 19:56:44 +0500

    passing requestObject to controller as param

--- src/utils/catchAsyncError.ts
+++ src/utils/catchAsyncError.ts
@@ -1,17 +1,28 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type IRequestObject = {
+    body: any;
+    query: any;
+    params: any;
+};
+
 type IApiResponse = {
     message: string;
-    // eslint-disable-next-line
     data: any;
     code: number;
 };
 
-type ControllerFunc = () => Promise<IApiResponse>;
+type ControllerFunc = (reqObject: IRequestObject) => Promise<IApiResponse>;
 
 function catchAsyncError(controller: ControllerFunc): RequestHandler {
-    return function (_req: Request, res: Response, next: NextFunction) {
-        controller()
+    return function (req: Request, res: Response, next: NextFunction) {
+        const reqObject = {
+            body: req.body,
+            params: req.params,
+            query: req.query,
+        };
+        controller(reqObject)
             .then((resp) => {
                 res.status(resp.code).json({
                     status: "success",
