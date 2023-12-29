27b61e7a6a0902eb6e4329f51fc1e78054b181af
mrx7355608 <mrx7355608@gmail.com>
2023-12-29 13:04:35 +0500

    setting up passport

---
+++ src/passport.ts
@@ -0,0 +1,25 @@
+import passport from "passport"
+import { Strategy } from "passport-local"
+import UserData from "./data/user.data";
+import { IUser } from "./types/user.types";
+
+const usersDB = UserData();
+
+export default function passportSetup() {
+    passport.use(new Strategy(async (username: string, password: string, done: any) => {
+        // validate username and password inputs
+        // check if admin exists
+        // validate password
+        // send back admin data
+    }))
+
+    passport.serializeUser((user: IUser, done: any) => done(null, user.id));
+    passport.deserializeUser(async (id: string, done: any) => {
+        try {
+            const user = await usersDB.findById(id);
+            return done(null, user);
+        } catch (err: any) {
+            return done(err);
+        }
+    })
+}
