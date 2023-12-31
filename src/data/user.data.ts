851c426e42d85ca1f3f9a2ef145202bfb8f48bf6
mrx7355608 <mrx7355608@gmail.com>
2023-12-31 13:30:48 +0500

    added a function for searching admin by his username

--- src/data/user.data.ts
+++ src/data/user.data.ts
@@ -1,5 +1,5 @@
-import UserModel from "../models/user.model"
-import { IUser } from "../types/user.types"
+import UserModel from "../models/user.model";
+import { IUser } from "../types/user.types";
 
 export default function UserData() {
     async function findByID(id: string): Promise<IUser | null> {
@@ -7,5 +7,10 @@ export default function UserData() {
         return user;
     }
 
-    return { findByID }
+    async function findByUsername(username: string): Promise<IUser | null> {
+        const user = await UserModel.findOne({ username });
+        return user;
+    }
+
+    return { findByID, findByUsername };
 }
