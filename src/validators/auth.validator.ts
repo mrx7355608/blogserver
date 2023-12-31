5e21faafc2d8d3fe29296bdeec532a2c7ca4cf35
mrx7355608 <mrx7355608@gmail.com>
2023-12-31 13:31:06 +0500

    returning error messages instead of throwing them from auth.validator

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -1,23 +1,29 @@
-import joi from "joi"
-import {ILoginInput} from "../types/user.types"
-import {ApiError} from "../utils/ApiError";
+import joi from "joi";
+import { ILoginInput } from "../types/user.types";
+import { ApiError } from "../utils/ApiError";
 
-const loginDataValidationSchema = joi.object({
-    username: joi.string().required().messages({
-        "any.required": "Please enter your username",
-        "string.empty": "Please enter your username",
-        "string.base": "Invalid username",
-    }),
-    password: joi.string().required().messages({
-        "any.required": "Please enter your password",
-        "string.empty": "Please enter your password",
-        "string.base": "Invalid password",
-    }),
-}).messages({
-    "object.unknown": "Unknown field detected, cannot login",
-})
+const loginDataValidationSchema = joi
+    .object({
+        username: joi.string().required().messages({
+            "any.required": "Please enter your username",
+            "string.empty": "Please enter your username",
+            "string.base": "Invalid username",
+        }),
+        password: joi.string().required().messages({
+            "any.required": "Please enter your password",
+            "string.empty": "Please enter your password",
+            "string.base": "Invalid password",
+        }),
+    })
+    .messages({
+        "object.unknown": "Unknown field detected, cannot login",
+    });
 
 export default function loginDataValidator(data: ILoginInput) {
     const { error } = loginDataValidationSchema.validate(data);
-    if (error) throw new ApiError(error.message, 400);
+    if (error) {
+        return error.message;
+    } else {
+        return null;
+    }
 }
