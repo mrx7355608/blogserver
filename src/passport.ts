06ab328a85e40d715c9542878c592b468bd98608
mrx7355608 <mrx7355608@gmail.com>
2023-12-31 13:31:27 +0500

    wrote signin logic for admin authentication

--- src/passport.ts
+++ src/passport.ts
@@ -1,20 +1,47 @@
-import passport from "passport"
-import { Strategy } from "passport-local"
+import passport from "passport";
+import { Strategy } from "passport-local";
 import UserData from "./data/user.data";
-import { IUser } from "./types/user.types";
+import bcrypt from "bcryptjs";
+import loginDataValidator from "./validators/auth.validator";
 
 const usersDB = UserData();
 
 export default function passportSetup() {
-    passport.use(new Strategy(async (username: string, password: string, done: any) => {
-        // validate username and password inputs
-        // check if admin exists
-        // validate password
-        // send back admin data
-    }))
+    passport.use(
+        // eslint-disable-next-line
+        new Strategy(async (username: string, password: string, done: any) => {
+            // validate username and password inputs
+            const errorMessage = loginDataValidator({ username, password });
+            if (errorMessage) {
+                return done(null, false, { message: errorMessage });
+            }
+
+            // check if admin exists
+            const admin = await usersDB.findByUsername(username);
+            if (admin === null) {
+                return done(null, false, {
+                    message: "Incorrect username or password",
+                });
+            }
+
+            // validate password
+            const isValidPassword = await bcrypt.compare(
+                password,
+                admin.password,
+            );
+            if (isValidPassword === false) {
+                return done(null, false, {
+                    message: "Incorrect username or password",
+                });
+            }
+
+            // send back admin data
+            return done(admin);
+        }),
+    );
 
     passport.serializeUser((user: any, done: any) => {
-        return done(null, user.id)
+        return done(null, user.id);
     });
     passport.deserializeUser(async (id: string, done: any) => {
         try {
@@ -23,5 +50,5 @@ export default function passportSetup() {
         } catch (err: any) {
             return done(err);
         }
-    })
+    });
 }
