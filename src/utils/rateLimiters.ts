aa58c26ccc6b5335192d6db39dbd39c2cfb9baa9
mrx7355608 <mrx7355608@gmail.com>
2023-12-31 20:30:01 +0500

    created a function to send custom json response when rate limit exceeds

--- src/utils/rateLimiters.ts
+++ src/utils/rateLimiters.ts
@@ -1,18 +1,33 @@
+import { Request, Response } from "express";
 import rateLimit from "express-rate-limit";
+
+function rateLimiterResponse(_req: Request, res: Response) {
+    // Customize the rate limit message here
+    const jsonResponse = {
+        status: "failed",
+        message: "Rate limit exceeded",
+        data: null,
+    };
+    // Set the response status and content type
+    res.status(429).json(jsonResponse);
+}
 
 const adminLoginLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
     max: 5,
+    message: rateLimiterResponse,
 });
 
 const adminBlogActionsLimiter = rateLimit({
     windowMs: 60 * 1000,
     max: 20,
+    message: rateLimiterResponse,
 });
 
 const adminReadOnlyLimiter = rateLimit({
     windowMs: 60 * 1000,
     max: 50,
+    message: rateLimiterResponse,
 });
 
 export { adminLoginLimiter, adminReadOnlyLimiter, adminBlogActionsLimiter };
